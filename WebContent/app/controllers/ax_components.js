f0abef0e886e1bbd18011132323354ef30adede6
Krantikc <kranti.chavannavar@gmail.com>
2015-10-10 02:25:16 +0530

    clean

--- WebContent/app/controllers/ax_components.js
+++ WebContent/app/controllers/ax_components.js
@@ -1,37 +1,28 @@
 axrControllers
 .controller('ComponentsController', 
     function($scope, $rootScope,  $stateParams, axLocale, $state, componentsAPIService) {
-	
-	var isLogin = $rootScope.loginCheck();
+
+    var isLogin = $rootScope.loginCheck();
 
     if (!isLogin) {
-    	 $state.go('login');
+         $state.go('login');
     }
-        
-    // Localizations    
-    $scope.home = axLocale.translate('home');
-    $scope.about = axLocale.translate('about');
-    $scope.products = axLocale.translate('products');
-    $scope.warranty = axLocale.translate('warranty');
-    $scope.dealers = axLocale.translate('dealers');
-    $scope.contactUs = axLocale.translate('contactUs');
     
+    $scope.getComponents = function() {
+        componentsAPIService.getComponents().success(function(response) {
+            if (response.success) {
+                $scope.components = response.components;
+            }
+        });
+    };
 
-	$scope.getComponents = function() {
-		componentsAPIService.getComponents().success(function(response) {
-			if (response.success) {
-				$scope.components = response.components;
-			}
-		});
-	};
-	
-	$scope.getComponents();
-	
-	$scope.updateComponent = function(component) {
-		componentsAPIService.updateComponent(component).success(function(response) {
-			if (response.success) {
-				$scope.getComponents();
-			}
-		});
-	};
+    $scope.getComponents();
+
+    $scope.updateComponent = function(component) {
+        componentsAPIService.updateComponent(component).success(function(response) {
+            if (response.success) {
+                $scope.getComponents();
+            }
+        });
+    };
 });
