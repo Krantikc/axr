d90f9d92f58967c4534b3d1cb5a264b987c6853f
Krantikc <kranti.chavannavar@gmail.com>
2015-10-10 02:42:59 +0530

    clean

--- WebContent/app/controllers/ax_blocks.js
+++ WebContent/app/controllers/ax_blocks.js
@@ -1,30 +1,28 @@
 axrControllers
 .controller('BlocksController', 
     function($scope, $rootScope, axLocale, $state, blocksAPIService) {
-	
-	var isLogin = $rootScope.loginCheck();
+
+    var isLogin = $rootScope.loginCheck();
 
     if (!isLogin) {
-    	 $state.go('login');
+         $state.go('login');
     }
-		// Localizations
 
-	$scope.getBlocks = function() {
-		blocksAPIService.getBlocks().success(function(response) {
-			if (response.success) {
-				$scope.blocks = response.blocks;
-			}
-		});
-	};
-	
-	$scope.getBlocks();
-	
-	$scope.updateBlock = function(block) {
-		blocksAPIService.updateBlock(block).success(function(response) {
-			if (response.success) {
-				$scope.getBlocks();
-			}
-		});
-	};
-	
+    $scope.getBlocks = function() {
+        blocksAPIService.getBlocks().success(function(response) {
+            if (response.success) {
+                $scope.blocks = response.blocks;
+            }
+        });
+    };
+
+    $scope.getBlocks();
+
+    $scope.updateBlock = function(block) {
+        blocksAPIService.updateBlock(block).success(function(response) {
+            if (response.success) {
+                $scope.getBlocks();
+            }
+        });
+    };
 });
